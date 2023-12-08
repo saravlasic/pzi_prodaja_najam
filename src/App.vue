240ee58d523e7e59fab4e3971f0576c5de03403f
Sara <sara.vlasic@fpmoz.sum.ba>
2023-12-08 19:40:42 +0100

    prijava

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Prodaja/najam haljina</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn @click="prikaziPrijavu">Prijava</v-btn>
+    </v-app-bar>
+    <v-dialog v-model="dialogPrijave" max-width="500px">
+      <v-card>
+        <v-card-title>Prijava</v-card-title>
+        <v-card-text>
+          <v-form @submit.prevent="obradiPrijavu">
+            <!-- Dodajte polja za unos podataka za prijavu -->
+            <v-text-field v-model="korisnickoIme" label="Korisničko ime"></v-text-field>
+            <v-text-field v-model="lozinka" label="Lozinka" type="password"></v-text-field>
+                <v-btn type="submit">Prijavi se</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      dialogPrijave: false,
+      korisnickoIme: '',
+      lozinka: ''
+    };
+  },
+  methods: {
+    prikaziPrijavu() {
+      // Prikazivanje prijavnog prozora
+      this.dialogPrijave = true;
+    },
+    obradiPrijavu() {
+      // Implementacija logike prijave
+      console.log('Podaci za prijavu:', this.korisnickoIme, this.lozinka);
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+      // Zatvorite prijavni prozor
+      this.dialogPrijave = false;
+    }
+  }
+};
+</script>
