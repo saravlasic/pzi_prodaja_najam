541be9e7aad7ac23892a23ec53771d9421f77715
Sara <sara.vlasic@fpmoz.sum.ba>
2023-12-09 17:29:23 +0100

    registracija

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,11 @@
         app
     >
       <!--  -->
+      <v-tabs v-model="activeTab" slider-color="primary">
+        <v-tab v-for="(item, index) in tabs" :key="index" @click="activeTab = index">
+          {{ item }}
+        </v-tab>
+      </v-tabs>
     </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -14,13 +19,29 @@
     </v-app-bar>
     <v-dialog v-model="dialogPrijave" max-width="500px">
       <v-card>
-        <v-card-title>Prijava</v-card-title>
+        <v-card-title v-if="!registracijaMode">Prijava</v-card-title>
+        <v-card-title v-else>Registracija</v-card-title>
         <v-card-text>
-          <v-form @submit.prevent="obradiPrijavu">
+          <v-form v-if="!registracijaMode" @submit.prevent="obradiPrijavu">
             <!-- Dodajte polja za unos podataka za prijavu -->
             <v-text-field v-model="korisnickoIme" label="Korisničko ime"></v-text-field>
             <v-text-field v-model="lozinka" label="Lozinka" type="password"></v-text-field>
-                <v-btn type="submit">Prijavi se</v-btn>
+            <p style="font-size: 0.8em; margin-top: 8px;">
+              Potreban vam je račun? <a href="#" @click="prikaziRegistraciju">Registrirajte se</a>
+            </p>
+            <v-btn type="submit">Prijavi se</v-btn>
+          </v-form>
+          <v-form v-else @submit.prevent="obradiRegistraciju">
+            <!-- Dodajte polja za registraciju -->
+            <v-text-field v-model="ime" label="Ime"></v-text-field>
+            <v-text-field v-model="prezime" label="Prezime"></v-text-field>
+            <v-text-field v-model="brojTelefona" label="Broj telefona"></v-text-field>
+            <v-text-field v-model="email" label="Email"></v-text-field>
+            <v-text-field v-model="novaLozinka" label=" Lozinka" type="password"></v-text-field>
+            <v-btn type="submit">Registrirajte se</v-btn>
+            <p style="font-size: 0.8em; margin-top: 8px;">
+              Već imate račun? <a href="#" @click="prikaziPrijavu">Prijavite se</a>
+            </p>
           </v-form>
         </v-card-text>
       </v-card>
@@ -37,19 +58,33 @@ export default {
     return {
       dialogPrijave: false,
       korisnickoIme: '',
-      lozinka: ''
+      lozinka: '',
+      ime: '',
+      prezime: '',
+      brojTelefona: '',
+      email: '',
+      novaLozinka: '',
+      drawer: false,
+      tabs: ['Prodaja','Najam'],
+      activeTab: null,
+      registracijaMode: false
     };
   },
   methods: {
     prikaziPrijavu() {
-      // Prikazivanje prijavnog prozora
+      this.dialogPrijave = true;
+      this.registracijaMode = false;
+    },
+    prikaziRegistraciju() {
+      this.registracijaMode = true;
       this.dialogPrijave = true;
     },
     obradiPrijavu() {
-      // Implementacija logike prijave
       console.log('Podaci za prijavu:', this.korisnickoIme, this.lozinka);
-
-      // Zatvorite prijavni prozor
+      this.dialogPrijave = false;
+    },
+    obradiRegistraciju() {
+      console.log('Podaci za registraciju:', this.ime, this.prezime, this.brojTelefona, this.email, this.novaLozinka);
       this.dialogPrijave = false;
     }
   }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <div>
-    </div>
-  </div>
+  <v-app>
+    <v-main class="background">
+      <!-- Tvoj sadržaj ovdje -->
+    </v-main>
+  </v-app>
+
 </template>
+<style>
+.background {
+  background-image: url('/public/slike/poz.jpg'); /* Postavi putanju do slike */
+  background-size: cover; /* Postavi veličinu slike */
+  background-position: center center; /* Pozicioniraj sliku */
+}
+</style>
 
 
 
